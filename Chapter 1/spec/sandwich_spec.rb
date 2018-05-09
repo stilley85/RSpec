f9a7973258c65f2597ff44a492664b1ea5279693
stephen <stephentilley@gmail.com>
2018-05-09 11:11:54 +0100

    :white_check_mark: test: Tested using helper method and let

--- Chapter 1/spec/sandwich_spec.rb
+++ Chapter 1/spec/sandwich_spec.rb
@@ -23,17 +23,55 @@ Sandwich = Struct.new(:taste, :toppings)
 
 # Using hooks
 
+# RSpec.describe 'An ideal sandwich' do
+#   before { @sandwich = Sandwich.new('delicious', []) }
+#   it 'is delicious' do
+#     taste = @sandwich.taste
+#
+#     expect(taste).to eq('delicious')
+#   end
+#
+#   it 'lets me add toppings' do
+#     @sandwich.toppings << 'cheese'
+#     toppings = @sandwich.toppings
+#
+#     expect(toppings).not_to be_empty
+#   end
+# end
+
+# Using helper methods
+
+# RSpec.describe 'An ideal sandwich' do
+#   def sandwich
+#     @sandwich ||= Sandwich.new('delicious', [])
+#   end
+#   it 'is delicious' do
+#     taste = sandwich.taste
+#
+#     expect(taste).to eq('delicious')
+#   end
+#
+#   it 'lets me add toppings' do
+#     sandwich.toppings << 'cheese'
+#     toppings = sandwich.toppings
+#
+#     expect(toppings).not_to be_empty
+#   end
+# end
+
+# Using let
+
 RSpec.describe 'An ideal sandwich' do
-  before { @sandwich = Sandwich.new('delicious', []) }
+  let(:sandwich) { Sandwich.new('delicious', []) }
   it 'is delicious' do
-    taste = @sandwich.taste
+    taste = sandwich.taste
 
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    @sandwich.toppings << 'cheese'
-    toppings = @sandwich.toppings
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
 
     expect(toppings).not_to be_empty
   end
