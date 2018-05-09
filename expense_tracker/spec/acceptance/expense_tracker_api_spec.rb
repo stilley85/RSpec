c5e7b7e848eaab93ba4d7644b0cf56a36f3c7cd3
stephen <stephentilley@gmail.com>
2018-05-10 00:34:14 +0100

    :white_check_mark: test: Test response body

--- expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
+++ expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -20,5 +20,18 @@ module ExpenseTracker
       post '/expenses', JSON.generate(coffee)
       expect(last_response.status).to eq(200)
     end
+
+    it 'records submitted expenses' do
+      coffee = {
+        'payee'  => 'Starbucks',
+        'amount' => 5.75,
+        'date'   => '2017-06-10'
+      }
+      post '/expenses', JSON.generate(coffee)
+      expect(last_response.status).to eq(200)
+
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+    end
   end
 end
