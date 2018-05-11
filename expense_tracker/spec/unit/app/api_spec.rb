f578b4014a4849aff4a1eb2f5382c9810fba3ef4
stephen <stephentilley@gmail.com>
2018-05-11 17:01:44 +0100

    :white_check_mark: test: API responds with a 200 on success

--- expense_tracker/spec/unit/app/api_spec.rb
+++ expense_tracker/spec/unit/app/api_spec.rb
@@ -27,8 +27,18 @@ module ExpenseTracker
           parsed = JSON.parse(last_response.body)
           expect(parsed).to include('expense_id' => 417)
         end
-        
-        it 'responds with a 200 (OK)'
+
+        it 'responds with a 200 (OK)' do
+          expense = { 'some' => 'data' }
+
+          allow(ledger).to receive(:record)
+          .with(expense)
+          .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
+
       end
 
       context 'when the expense fails validation' do
