fcf3f58055d3e14163987f2b4245ddd3e17f42a5
stephen <stephentilley@gmail.com>
2018-05-12 23:15:04 +0100

    :white_check_mark: test: Test GET response for valid expense date

--- expense_tracker/spec/unit/app/api_spec.rb
+++ expense_tracker/spec/unit/app/api_spec.rb
@@ -9,6 +9,10 @@ module ExpenseTracker
       API.new(ledger: ledger)
     end
 
+    def parse_last_response
+      JSON.parse(last_response.body)
+    end
+
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
 
     describe 'POST /expenses' do
@@ -24,8 +28,7 @@ module ExpenseTracker
         it 'returns the expense id' do
           post '/expenses', JSON.generate(expense)
 
-          parsed = JSON.parse(last_response.body)
-          expect(parsed).to include('expense_id' => 417)
+          expect(parse_last_response).to include('expense_id' => 417)
         end
 
         it 'responds with a 200 (OK)' do
@@ -47,14 +50,38 @@ module ExpenseTracker
         it 'returns an error message' do
           post '/expenses', JSON.generate(expense)
 
-          parsed = JSON.parse(last_response.body)
-          expect(parsed).to include('error' => 'Expense incomplete')
+          expect(parse_last_response).to include('error' => 'Expense incomplete')
         end
 
         it ' responds with a 422 (Unprocessable entity)' do
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(422)
         end
+      end
+    end
+
+    describe 'GET /expenses/:date' do
+      context 'when expenses exist on a given date' do
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with('2017-06-10')
+            .and_return(['expense_1', 'expense_2'])
+        end
+
+        it 'returns the expense records as JSON' do
+          get '/expenses/2017-06-10'
+          expect(parse_last_response).to eq(['expense_1', 'expense_2'])
+        end
+
+        it 'responds with a 200 (OK)' do
+          get '/expenses/2017-06-10'
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when there are no expenses on a given date' do
+        it 'returns an empty array as JSON'
+        it 'responds with a 200 (OK)'
       end
     end
   end
