68ea02efb9d3bfc7001bd3ca2a1d89bd8a618012
stephen <stephentilley@gmail.com>
2018-05-15 10:17:15 +0100

    :sparkles: feat: Ledger exenses_on method implemented

--- expense_tracker/app/ledger.rb
+++ expense_tracker/app/ledger.rb
@@ -7,13 +7,14 @@ module ExpenseTracker
         message = 'Invalid expense: `payee` is required'
         return RecordResult.new(false, nil, message)
       end
-      
+
       DB[:expenses].insert(expense)
       id = DB[:expenses].max(:id)
       RecordResult.new(true, id, nil)
     end
 
     def expenses_on(date)
+      DB[:expenses].where(date: date).all
     end
   end
 end
